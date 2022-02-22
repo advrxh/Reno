e20781c8e4599a14dc8530af7606766d98f6b9b3
Aadil Varsh <aadilvarshofficial@gmail.com>
2022-02-22 22:13:42 +0530

    Remove personal webhook link

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,5 @@
 const hooks = [
     // insert all of your discord webhook links
-    "https://discord.com/api/webhooks/945713274618540133/KuLK-WhBs2kXN5OlSdJI-_KLC3cbRZrjwXGbQqwvFx7_9sBlVD9z8x6LqvfOpOoBn5IH",
 ];
 
 export default hooks;
